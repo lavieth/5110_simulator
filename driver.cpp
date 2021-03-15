57dd3fc57373f039bf193ad5499f953dfab96da2
PeterPogo <peterpogo234@gmail.com>
2021-03-15 16:57:41 -0500

    Added header, basic structure

--- driver.cpp
+++ driver.cpp
@@ -1,8 +1,29 @@
+// Iterative_Method_Simulator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+///////////////////////////////////////////////////////////////////////////////////////
+// Desc: Iterative Method Simulator                                                  //
+// Class: CpE 5110                                                                   //
+// Instructor: Dr. Ali Hurson                                                        //
+// Date: 3/15/2021                   SP21                                            //
+// Piotr Pogorzelski, Levi Vieth                                                     //
+///////////////////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <iomanip> 
+#include <cmath>
+#include <algorithm>
+#include <chrono>
+#include <thread>
+#include "driver.h"
 
-using namespace std;
-
+using namespace std; 
+using namespace std::this_thread;     // sleep_for, sleep_until
+using namespace std::chrono_literals; // ns, us, ms, s, h, etc.
+using std::chrono::system_clock;
 
 int main(){
 
@@ -11,6 +32,4 @@ int main(){
 
 
 // Hello 2
-// Hello 4
-// hello
 }
---
+++ driver.h
@@ -0,0 +1,11 @@
+#pragma once
+// Declare functions here
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+// General functions
+void simulate_IM(int inst_count); // Simulate Iterative Method (IM)
